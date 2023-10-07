d249fbfa8a0963f2ab35151fbea767221d537eb6
delsagittar <106860464+delsagittar@users.noreply.github.com>
2023-10-06 21:45:54 -0400

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 
 # Set the GPIO mode and pin number
 GPIO.setmode(GPIO.BCM)  # Set the GPIO pin numbering mode to BCM
-led_pin = 17  # Define the GPIO pin number to which the LED is connected
+forward = 17  # Define the GPIO pin number to which the LED is connected
+backward= 27
+left=23
+right=24
 GPIO.setup(led_pin, GPIO.OUT)  # Set the LED pin as an output pin
 
 # Define routes
@@ -17,8 +20,24 @@ def index():
 
 @app.route("/turn_on")
 def turn_on():
-    GPIO.output(led_pin, GPIO.HIGH)  # Turn on the LED by setting the pin to HIGH
-    return "LED turned on"  # Return a message indicating that the LED is turned on
+    GPIO.output(forward, GPIO.HIGH)  # Turn on the LED by setting the pin to HIGH
+    return "moving forward"  # Return a message indicating that the LED is turned on
+
+@app.route("/left")
+def left():
+    GPIO.output(left, GPIO.HIGH)  # Turn on the LED by setting the pin to HIGH
+    return "left"  # Return a message indicating that the LED is turned on
+
+@app.route("/back")
+def back():
+    GPIO.output(backward, GPIO.HIGH)  # Turn on the LED by setting the pin to HIGH
+    return "back"  # Return a message indicating that the LED is turned on
+
+@app.route("/right")
+def right():
+    GPIO.output(right, GPIO.HIGH)  # Turn on the LED by setting the pin to HIGH
+    return "right"  # Return a message indicating that the LED is turned on
+
 
 @app.route("/turn_off")
 def turn_off():
